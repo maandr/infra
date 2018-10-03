f5861aee70295eb364b9caaf39aa975ef8c5e93f
Mats Andresen <mats.andresen@gmx.de>
2018-10-03 12:40:55 +0200

    [infra] make script sh compliant.

--- build-machine/provsion-mysql-debian.sh
+++ build-machine/provsion-mysql-debian.sh
@@ -6,47 +6,44 @@ yellow=$(tput setaf 3)
 normal=$(tput sgr0)
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+project_dir=$(dirname "${script_dir}")
 
-pushd ${script_dir} > /dev/null
+. ${project_dir}/.secrets
 
-    source ../.secrets
+printf "\n\n"
+printf "${yellow}uninstall any existing mysql packages..${normal}"
+apt-get remove --purge mysql\*
+apt-get clean
+updatedb
 
-    printf "\n\n"
-    printf "${yellow}uninstall any existing mysql packages..${normal}"
-    apt-get remove --purge mysql\*
-    apt-get clean
-    updatedb
+printf "\n\n"
+printf "${yellow}install mysql-client..${normal}"
+apt-get install -y libmysqlclient-dev mysql-client
 
-    printf "\n\n"
-    printf "${yellow}install mysql-client..${normal}"
-    apt-get install -y libmysqlclient-dev mysql-client
+printf "\n\n"
+printf "${yellow}install mysql-server..${normal}"
+spt-get update
+apt-get install -y mysql-server-5.6
+mysql --version
 
-    printf "\n\n"
-    printf "${yellow}install mysql-server..${normal}"
-    spt-get update
-    apt-get install -y mysql-server-5.6
-    mysql --version
+printf "\n\n"
+printf "${yellow}securing mysql-server..${normal}"
 
-    printf "\n\n"
-    printf "${yellow}securing mysql-server..${normal}"
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('${mysql_password}') WHERE User = 'root'"
+mysql -e "DROP USER ''@'localhost'"
+mysql -e "DROP USER ''@'$(hostname)'"
+mysql -e "DROP DATABASE test"
+mysql -e "FLUSH PRIVILEGES"
 
-    mysql -e "UPDATE mysql.user SET Password = PASSWORD('${mysql_password}') WHERE User = 'root'"
-    mysql -e "DROP USER ''@'localhost'"
-    mysql -e "DROP USER ''@'$(hostname)'"
-    mysql -e "DROP DATABASE test"
-    mysql -e "FLUSH PRIVILEGES"
+printf "\n\n"
+printf "${yellow}testing mysql service..${normal}"
+systemctl status mysql.service
 
-    printf "\n\n"
-    printf "${yellow}testing mysql service..${normal}"
-    systemctl status mysql.service
+printf "\n\n"
+printf "${yellow}connect to mysql service..${normal}"
+mysql -u ${mysql_root_user} -p${mysql_root_password} \
+    -e "SELECT host, user from mysql.user;"
 
-    printf "\n\n"
-    printf "${yellow}connect to mysql service..${normal}"
-    mysql -u ${mysql_root_user} -p${mysql_root_password} \
-        -e "SELECT host, user from mysql.user;"
-
-    printf "\n\n"
-    printf "verifying port is open to allow remote access to mysql.."
-    nc -vz ${mysql_hostname} ${mysql_port}
-
-popd > /dev/null
+printf "\n\n"
+printf "verifying port is open to allow remote access to mysql.."
+nc -vz ${mysql_hostname} ${mysql_port}
