0645ea41d2e995ea48849a8edf0225880b03f908
Mats Andresen <mats.andresen@gmx.de>
2018-10-03 21:02:15 +0200

    [certbot] add certbot run-script.

--- build-machine/request-ssl-certificates.sh
+++ build-machine/request-ssl-certificates.sh
@@ -3,6 +3,17 @@
 script_dir=$(dirname $(readlink -f "$0"))
 project_dir="$(dirname ${script_dir})"
 
-
+docker run -it --rm \
+    -v /docker-volumes/certs:/etc/letsencrypt \
+    -v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
+    -v /docker-volumes/var/log/letsencrypt:/var/log/letsencrypt \
+    -v /static:/data/letsencrypt \
+    certbot/certbot \
+    certonly --webroot \
+    --register-unsafely-without-email --agree-tos \
+    --webroot-path=/data/letsencrypt \
+    --staging \
+    -d maandr.de
+    -d www.maandr.de
 
 exit 0
