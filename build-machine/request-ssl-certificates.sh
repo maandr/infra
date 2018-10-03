a735ecff6d129b2b8118a555d89b4b63667bec01
Mats Andresen <mats.andresen@gmx.de>
2018-10-03 23:03:48 +0200

    [certbot] real-deal.

--- build-machine/request-ssl-certificates.sh
+++ build-machine/request-ssl-certificates.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+yellow=$(tput setaf 3)
+normal=$(tput sgr0)
+
 script_dir=$(dirname $(readlink -f "$0"))
 project_dir="$(dirname ${script_dir})"
+
+letsencrypt_admin_email=$($project_dir/util/secret-value.sh letsencrypt_admin_email)
+
+printf "\n\n"
+printf "${yellow}requesting ssl certificates..${normal}\n"
 
 docker run -it --rm \
     -v /docker-volumes/certs:/etc/letsencrypt \
@@ -10,10 +18,10 @@ docker run -it --rm \
     -v ${script_dir}/static:/static \
     certbot/certbot \
     certonly --webroot \
-    --register-unsafely-without-email --agree-tos \
+    --email ${letsencrypt_admin_email} --agree-tos \
     --webroot-path=/static \
-    --staging \
     -d maandr.de \
-    -d www.maandr.de
+    -d www.maandr.de \
+    -d ci.maandr.de
 
 exit 0
--- build-machine/start.sh
+++ build-machine/start.sh
@@ -6,8 +6,6 @@ normal=$(tput sgr0)
 script_dir=$(dirname $(readlink -f "$0"))
 compose_file="${script_dir}/docker-compose.yml"
 
-
-
 printf "\n\n"
 printf "${yellow}starting containers..${normal}\n"
 docker-compose --file ${compose_file} up -d
