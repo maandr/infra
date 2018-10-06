4ec35a6ffb36c21ec22dbc9964ea631a584b4325
Mats Andresen <mats.andresen@gmx.de>
2018-10-06 12:53:09 +0200

    [infra] add mysql backup script.

---
+++ build-machine/create-database-backup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+script_dir=$(dirname $(readlink -f "$0"))
+project_dir="$(dirname ${script_dir})"
+
+. ${project_dir}/.secrets
+
+datetime=`date '+%d-%m-%Y-%H-%M-%S'`
+
+docker exec mysql \
+    sh -c 'exec mysqldump --all-databases -uroot -p"${mysql_root_password}"' \
+    | gzip > "/var/lib/mysql/dumps/${datetime}.sql.gz"
