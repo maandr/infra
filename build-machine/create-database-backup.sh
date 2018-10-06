b7bf4c382232efb1f81566af916608cbb2d3a16e
Mats Andresen <mats.andresen@gmx.de>
2018-10-06 13:01:32 +0200

    [infra] fix db backup script.

--- build-machine/create-database-backup.sh
+++ build-machine/create-database-backup.sh
@@ -5,8 +5,8 @@ project_dir="$(dirname ${script_dir})"
 
 . ${project_dir}/.secrets
 
-datetime=`date '+%d-%m-%Y-%H-%M-%S'`
+datetime=`date "+%d-%m-%Y-%H-%M-%S"`
 
 docker exec mysql \
-    sh -c 'exec mysqldump --all-databases -uroot -p"${mysql_root_password}"' \
-    | gzip > "/var/lib/mysql/dumps/${datetime}.sql.gz"
+    sh -c "exec mysqldump --all-databases -uroot -p${mysql_root_password} \
+    | gzip > /var/lib/mysql/dumps/date ${datetime}.sql.gz"
