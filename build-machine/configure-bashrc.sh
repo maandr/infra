3b1b27a65f8371c51e39a6ddb40fd6d178393835
Mats Andresen <mats.andresen@gmx.de>
2018-10-07 18:30:08 +0200

    [infra] user bashrc over aliases.

--- build-machine/configure-bashrc.sh
+++ build-machine/configure-bashrc.sh
@@ -8,17 +8,17 @@ script_dir=$(dirname $(readlink -f "$0"))
 export INFRA_DIR="$(dirname ${script_dir})"
 export BUILD_MACHINE_DIR="${INFRA_DIR}/build-machine"
 
-bash_aliases=$(envsubst < "./.bash_aliases")
+bash_aliases=$(envsubst < "./.bashrc")
 
 printf "\n\n${blue}"
 echo "${bash_aliases}"
 printf "${normal}\n\n"
 
-read -p "${blue}Would you like me to override your existing ~/.bash_aliases with the above? [y/N]${normal}" -n 1 -r
+read -p "${blue}Would you like me to override your existing ~/.bashrc with the above? [y/N]${normal}" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo "${bash_aliases}" > ~/.bash_aliases
+    echo "${bash_aliases}" > ~/.bashrc
 fi
 
 exit 0
